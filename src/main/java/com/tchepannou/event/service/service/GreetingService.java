b64e44d230db5b1b3c4c3c3e9e0332b662b85855
htchepannou <htchepannou@expedia.com>
2015-09-13 13:44:56 -0400

    Initial

---
+++ src/main/java/com/tchepannou/event/service/service/GreetingService.java
@@ -0,0 +1,5 @@
+package com.tchepannou.event.service.service;
+
+public interface GreetingService {
+    String say (String word);
+}
---
+++ src/main/java/com/tchepannou/event/service/service/impl/GreetingServiceImpl.java
@@ -0,0 +1,9 @@
+package com.tchepannou.event.service.service.impl;
+
+import com.tchepannou.event.service.service.GreetingService;
+
+public class GreetingServiceImpl implements GreetingService {
+    public String say(String word) {
+        return word;
+    }
+}
